6a368c7029362f13b4478f71fe3d99b8c2eafafc
ktssvarma <sashanka8888@gmail.com>
2024-03-26 16:21:10 +0530

    26/3/2024

---
+++ add2numswithoutOperators.js
@@ -0,0 +1,14 @@
+var getSum = function(a, b) {
+    while (b) {
+        console.log(`${a}-->`, a.toString(2));
+        console.log(`${b}-->`, b.toString(2));
+        console.log(`${a} & ${b}`, (a & b).toString(2));
+        console.log(`${a} ^ ${b}`, (a ^ b).toString(2));
+        const carry = a & b;
+        a = a ^ b;
+        b = carry << 1;
+      }
+      return a;
+};
+
+console.log(getSum(5, 6));
--- anagram.js
+++ anagram.js
@@ -1,9 +1,25 @@
-var formatString = e => e.split('').sort().join('')
-var isAnagram = (s, t) => {
-    if (s.length !== t.length) return false
-    else return formatString(s) === formatString(t)
-}
+// var formatString = e => e.split('').sort().join('')
+// var isAnagram = (s, t) => {
+//     if (s.length !== t.length) return false
+//     else return formatString(s) === formatString(t)
+// }
 
-console.log(isAnagram("anagram","nagaram"))
-console.log(isAnagram("anagr12am","nagara21m"))
-console.log(isAnagram("anagr1234am","nagar5678am"))
+var isAnagram = (s, t) => {
+  if (s.length !== t.length) return false;
+  let obj1 = {};
+  let obj2 = {};
+  for (let i = 0; i < s.length; i++) {
+    obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+    obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+  }
+  for (const key in obj1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isAnagram("anagram", "nagaram"));
+console.log(isAnagram("anagr12am", "nagara21m"));
+console.log(isAnagram("anagr1234am", "nagar5678am"));
