92114fadb8ad08acbbaf40f253572d8035dde3ac
ktssvarma <sashanka8888@gmail.com>
2024-03-26 16:22:21 +0530

    26/3/2024

--- add2numswithoutOperators.js
+++ add2numswithoutOperators.js
@@ -1,9 +1,9 @@
 var getSum = function(a, b) {
     while (b) {
-        console.log(`${a}-->`, a.toString(2));
-        console.log(`${b}-->`, b.toString(2));
-        console.log(`${a} & ${b}`, (a & b).toString(2));
-        console.log(`${a} ^ ${b}`, (a ^ b).toString(2));
+        console.log(`${a}        -->`, a.toString(2));
+        console.log(`${b}        -->`, b.toString(2));
+        console.log(`${a} & ${b} -->`, (a & b).toString(2));
+        console.log(`${a} ^ ${b} -->`, (a ^ b).toString(2));
         const carry = a & b;
         a = a ^ b;
         b = carry << 1;
